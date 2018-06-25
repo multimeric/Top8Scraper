2533e0613daf3f1a70bfe96a67bf867926034f16
Michael Milton <ttmigueltt@gmail.com>
2018-06-26 00:39:34 +1000

    Better error handling

--- top8scraper/scrape.py
+++ top8scraper/scrape.py
@@ -1,5 +1,6 @@
 from urllib.parse import urlparse, parse_qs
 import asyncio
+import sys
 import datetime
 
 import aiohttp
@@ -8,8 +9,9 @@ from sqlalchemy.engine import Engine
 from sqlalchemy.orm.session import Session
 from sqlalchemy import table, column, select, true, func
 from bs4 import BeautifulSoup
+import bs4
 from progress.bar import Bar
-from aiohttp import ClientSession
+from aiohttp import ClientSession, ServerDisconnectedError, ClientOSError
 
 from top8scraper import models
 
@@ -67,7 +69,7 @@ def newest_event() -> int:
     Calculates the newest event stored by top8
     """
     response = requests.get('http://mtgtop8.com/index')
-    soup = BeautifulSoup(response.text, 'lxml')
+    soup = BeautifulSoup(response.text, 'html5lib')
     recent_events = []
     for star in soup.select('td.O16'):
         link = star.find_parent('tr').select('a')[0]
@@ -115,6 +117,31 @@ def get_event_futures(start_id: int, end_id: int, db_session: Session, http_sess
         yield scrape_event(i, db_session, http_session, progress_bar)
 
 
+class MalformedMarkupException(Exception):
+    def __init__(self, tag: bs4.Tag, selector: str):
+        self.tag = tag.name
+        self.selector = selector
+
+    def __str__(self):
+        return f'MalformedMarkup: {self.tag}, {self.selector}'
+
+
+def select_one(soup: bs4.Tag, selector: str):
+    selection = soup.select(selector)
+    if len(selection) == 1:
+        return selection[0]
+    else:
+        raise MalformedMarkupException(soup, selector)
+
+
+def select_multiple(soup: bs4.Tag, selector: str):
+    selection = soup.select(selector)
+    if len(selection) < 1:
+        raise MalformedMarkupException(soup, selector)
+    else:
+        return selection
+
+
 async def scrape_event(event_id: int, db_session: Session, http_session: ClientSession, progress_bar: Bar):
     """
     Async function that scrapes an event asynchronously and adds it to the database
@@ -123,38 +150,49 @@ async def scrape_event(event_id: int, db_session: Session, http_session: ClientS
     :param http_session: The HTTP session to use for HTTP requests
     :param progress_bar: The progress bar to update when a task finishes
     """
-    async with http_session.get(EVENT_URL, params={'e': event_id}) as response:
-        soup = BeautifulSoup(await response.text(), 'lxml')
 
-    # Get event metadata
-    meta = soup.select('td.S14')[0]
-    format_name = str(meta.contents[0]).strip()
-    player_date = str(meta.contents[2]).replace('players', '').split('-')
-    if len(player_date) == 2:
-        players = int(player_date[0].strip())
-        date = player_date[1]
-    else:
-        players = None
-        date = player_date[0]
+    while True:
+        try:
+            async with http_session.get(EVENT_URL, params={'e': event_id}) as response:
+                soup = BeautifulSoup(await response.text(), 'html5lib')
+                break
+        except (ServerDisconnectedError, ClientOSError):
+            print(f"Server disconnected for event {event_id}. Sleeping for 1 second.", file=sys.stderr)
+            await asyncio.sleep(0.1)
 
-    date = datetime.datetime.strptime(date.strip(), '%d/%m/%y').date()
+    try:
+        # Get event metadata
+        meta = select_one(soup, 'td.S14')
+        format_name = str(meta.contents[0]).strip()
+        player_date = str(meta.contents[2]).replace('players', '').split('-')
+        if len(player_date) == 2:
+            players = int(player_date[0].strip())
+            date = player_date[1]
+        else:
+            players = None
+            date = player_date[0]
 
-    event_name = soup.select('.S18')[0].text
+        date = datetime.datetime.strptime(date.strip(), '%d/%m/%y').date()
 
-    format = db_session.query(models.Format).filter(models.Format.name == format_name).first()
+        event_name = select_one(soup, '.S18').text
 
-    event = models.Event(
-        top8id=event_id,
-        date=date,
-        player_count=players,
-        name=event_name,
-        format=format
-    )
+        format = db_session.query(models.Format).filter(models.Format.name == format_name).first()
 
-    for deck in soup.select('div.S14 > a, div.W14 > a'):
-        await scrape_deck(deck.attrs['href'], event, db_session, http_session)
+        event = models.Event(
+            top8id=event_id,
+            date=date,
+            player_count=players,
+            name=event_name,
+            format=format
+        )
 
-    progress_bar.next()
+        for deck in select_multiple(soup, 'div.S14 > a, div.W14 > a'):
+            await scrape_deck(deck.attrs['href'], event, db_session, http_session)
+        progress_bar.next()
+
+    # except (MalformedMarkupException, IndexError):
+    except Exception as e:
+        print(f'Event {event_id} had malformed markup, failed with error {e}', file=sys.stderr)
 
 
 async def scrape_deck(deck_url: str, event: models.Event, db_session: Session, http_session: ClientSession):
@@ -165,30 +203,42 @@ async def scrape_deck(deck_url: str, event: models.Event, db_session: Session, h
     :param db_session: The database session to use for database interaction
     :param http_session: The HTTP session to use for HTTP requests
     """
-    async with http_session.get(EVENT_URL + deck_url) as response:
-        soup = BeautifulSoup(await response.text(), 'html.parser')
+    while True:
+        try:
+            async with http_session.get(EVENT_URL + deck_url) as response:
+                soup = BeautifulSoup(await response.text(), 'html5lib')
+                break
+        except (ServerDisconnectedError, ClientOSError):
+            print(f"Server disconnected for event {deck_url}. Sleeping for 1 second.", file=sys.stderr)
+            await asyncio.sleep(0.1)
 
-    # Scrape player metadata from sidebar
-    meta = soup.select('.chosen_tr')[0]
-    children = meta.select('> div')
-    rank = children[0].text.split('-')[0]
-    player = children[2].text.replace('"', '')
+    try:
+        # Scrape player metadata from sidebar
+        meta = soup.select('.chosen_tr')[0]
+        children = meta.select('> div')
+        rank = children[0].text.split('-')[0]
+        player = children[2].text.replace('"', '')
 
-    player = get_or_create(db_session, models.Player, name=player)
-    deck = models.Deck(
-        player=player,
-        event=event,
-        rank=rank
-    )
-    db_session.add(deck)
-
-    # Scrape the cards in the deck
-    for card_el in soup.select('td.G14 > div'):
-        js = card_el.attrs['onclick']
-        card_id = js.split(',')[-2].replace("'", '')
-
-        entry = models.DeckEntry(
-            deck=deck,
-            card_id=int(card_id)
+        player = get_or_create(db_session, models.Player, name=player)
+        deck = models.Deck(
+            player=player,
+            event=event,
+            rank=rank
         )
-        db_session.add(entry)
+        db_session.add(deck)
+
+        # Scrape the cards in the deck
+        for card_el in soup.select('td.G14 > div'):
+            js = card_el.attrs['onclick']
+            card_id = js.split(',')[-2].replace("'", '')
+
+            entry = models.DeckEntry(
+                deck=deck,
+                card_id=int(card_id) if str.isnumeric(card_id) else -1
+            )
+            db_session.add(entry)
+    # except (MalformedMarkupException, IndexError):
+    #     print(f'Deck with URL {deck_url} had malformed markup')
+    except Exception as e:
+        print(f'Deck with URL {deck_url} had malformed markup, failed with error {e}', file=sys.stderr)
+
